44a035e2113b93c791cc90d2a27f1db0ad310460
hatopopo5656 <234927979+hatopopo5656@users.noreply.github.com>
2026-02-05 23:41:56 +0900

    production環境設定を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,27 +18,3 @@ categories = [
 categories.each do |name|
   Category.find_or_create_by(name: name)
 end
-Recipe.find_or_create_by(title: "お手軽サラダ") do |r|
-  r.description = <<~HTML
-    <h2>作り方</h2>
-    <p>レタスとトマトを洗い、オリーブオイルで和える</p>
-    <ul>
-      <li>塩 少々</li>
-      <li>胡椒 少々</li>
-    </ul>
-  HTML
-
-  r.grocery = <<~HTML
-    <ul>
-      <li>レタス 1個</li>
-      <li>トマト 2個</li>
-      <li>オリーブオイル 大さじ1</li>
-    </ul>
-  HTML
-
-  r.carbs = 5
-  r.fat = 10
-  r.protein = 3
-  r.salt = 1
-  r.category = Category.find_by(name: "糖質制限")
-end
